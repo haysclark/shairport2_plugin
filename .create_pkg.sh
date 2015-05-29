272e4d6038c1e86a7081aa401b0292e188737715
disaster123 <none>
2015-05-29 06:31:48 +0200

    fix pkg creation - replace version before creating zip

--- .create_pkg.sh
+++ .create_pkg.sh
@@ -11,11 +11,12 @@ fi
 
 rm -fv ShairTunes2.zip
 
+# correct version must already be included
+sed -i "s#<version>.*</version>#<version>${VER}</version>#" install.xml
+
 zip -r ShairTunes2.zip *.pm *.txt *.md *.xml helperBinaries
 
 CHK=$(sha1sum -b ShairTunes2.zip|awk '{print $1}')
 
 sed -i "s#<sha>.*</sha>#<sha>${CHK}</sha>#" public.xml
 sed -i "s#version=\"[0-9\.]*\" #version=\"${VER}\" #" public.xml
-
-sed -i "s#<version>0.12</version>#<version>${VER}</version>#" install.xml
